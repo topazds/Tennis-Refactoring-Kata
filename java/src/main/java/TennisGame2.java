c111b2654f595c597b4c9047704a339f81a56c4f
Padungsak Kloynimnuan <padungsak.k@kasikornbank.com>
2017-07-26 09:42:52 +0700

    refactor code 2

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -11,65 +11,39 @@ public class TennisGame2 implements TennisGame {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
-    private String convertSrt(int score){
-        if (P1point == 0)
-            return  "Love";
-        if (P1point == 1)
+
+    private String convertSrt(int score) {
+        if (score == 1)
             return "Fifteen";
-        if (P1point == 2)
+        if (score == 2)
             return "Thirty";
-        if (P1point ==3 )
-            return "Fourty";
+        if (score == 3)
+            return "Forty";
         return "Love";
     }
+
     public String getScore() {
         String score1 = convertSrt (P1point);
         String score2 = convertSrt (P2point);
-        String score="";
-       if (P1point == P2point && P1point < 4) {
+        String score = "";
+        if (P1point == P2point && P1point < 4) {
             score = String.format ("%s-All", score1);
         }
         if (P1point == P2point && P1point >= 3)
             score = "Deuce";
 
         if (P1point > 0 && P2point == 0) {
-            P1res=score1;
-            P2res = "Love";
-            score = String.format ("%s-%s", P1res, P2res);
+            score = String.format ("%s-%s", score1, score2);
         }
         if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
-            P1res = "Love";
-            score = String.format ("%s-%s", P1res, P2res);
+            score = String.format ("%s-%s", score1, score2);
         }
 
         if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = String.format ("%s-%s", P1res, P2res);
+            score = String.format ("%s-%s", score1, score2);
         }
         if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = String.format ("%s-%s", P1res, P2res);
+            score = String.format ("%s-%s", score1, score2);
         }
 
         if (P1point > P2point && P2point >= 3) {
